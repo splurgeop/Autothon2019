bdd512b0dcac146da4b7f144495a179e4d1ff789
santosh <santosh.bciit@gmail.com>
2019-09-26 11:57:45 +0530

    Added mobile Helper and Hook file

---
+++ Autothon2019/Helpers/Mobile/Enviornment/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+
+
+namespace Autothon2019.Helpers.Mobile.Enviornment
+{
+    public class Config
+    {
+        
+        public static string PlatformName
+            => ConfigurationManager.AppSettings["platformName"];
+        public static int TimeOut
+            => Convert.ToInt32(ConfigurationManager.AppSettings["timeOut"]);
+        public static string AppiumServerUrl
+            => ConfigurationManager.AppSettings["appiumServerUrl"];
+        public static string Name
+           => ConfigurationManager.AppSettings["name"];
+        public static string Username
+           => ConfigurationManager.AppSettings["username"];
+        public static string Password
+           => ConfigurationManager.AppSettings["password"];
+        public static string Browser
+           => ConfigurationManager.AppSettings["browsername"];
+    }
+}
+
---
+++ Autothon2019/Helpers/Mobile/Screens/BaseScreen.cs
@@ -0,0 +1,27 @@
+﻿
+using Autothon2019.Helpers.Mobile.Appium;
+
+namespace Autothon2019.Helpers.Mobile.Screens
+{
+    public class BaseScreen
+    {
+        public static MobileApplication App;
+
+        /// <summary>
+        /// Creates an instance of AndroidDriver.
+        /// </summary>
+        public static void InitializeSession()
+        {
+            App = new MobileApplication();
+            App.StartSession();
+        }
+
+        /// <summary>
+        /// closes the Appium session.
+        /// </summary>
+        public static void DeleteSession()
+        {
+            App.EndSession();
+        }
+    }
+}
